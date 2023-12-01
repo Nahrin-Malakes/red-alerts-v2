897ee7072881a6d40ef713fd00e96b8d5c151210
Nahrin-Malakes <nahrinmalakes12@gmail.com>
2023-12-01 15:13:21 +0200

    Headers might block request

--- src/app/api/alerts/history/route.ts
+++ src/app/api/alerts/history/route.ts
@@ -7,10 +7,6 @@ export const GET = async () => {
   const alarms = await fetch(
     "https://www.oref.org.il//Shared/Ajax/GetAlarmsHistory.aspx?lang=en&mode=1",
     {
-      headers: {
-        "X-Requested-With": "XMLHttpRequest",
-        Referrer: "https://www.oref.org.il/",
-      },
       cache: "no-cache",
     }
   );
