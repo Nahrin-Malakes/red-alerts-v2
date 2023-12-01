72eef71384d533897d9ff43093f1d7bf0feda4d8
Nahrin-Malakes <nahrinmalakes12@gmail.com>
2023-12-01 15:10:00 +0200

    maybe fix

--- src/app/api/alerts/history/route.ts
+++ src/app/api/alerts/history/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 export const GET = async () => {
   const alarms = await fetch(
     "https://www.oref.org.il//Shared/Ajax/GetAlarmsHistory.aspx?lang=en&mode=1",
